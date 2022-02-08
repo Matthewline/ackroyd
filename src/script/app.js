1d858d8b1806aab181496e1477cff1d9e7cdf4f0
Matthew <matthewcode04@gmail.com>
2022-02-08 10:32:50 +0100

    changes at html css and fix API

--- src/script/app.js
+++ src/script/app.js
@@ -12,13 +12,13 @@ document.addEventListener("scroll", function () {
 
 // Newsletter
 const text = document.querySelector(".text");
-const email = document.querySelector(".form_email");
+const input = document.querySelector(".form_email");
 const button = document.querySelector(".form_button");
 let regExp = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
-email.addEventListener("input", function () {
-  if (email.value.match(regExp)) {
-    text.textContent = "Your address email is valid";
+input.addEventListener("input", function () {
+  if (input.value.match(regExp)) {
+    text.textContent = "Your address input is valid";
     text.classList.add("text_valid");
     text.classList.remove("text_invalid");
     button.addEventListener(
@@ -29,27 +29,33 @@ email.addEventListener("input", function () {
       },
       { once: true }
     );
-  } else if (email.value === "" || email.value == null) {
+  } else if (input.value === "" || input.value == null) {
     text.textContent = "";
   } else {
-    text.textContent = "Please enter valid email address";
+    text.textContent = "Please enter valid input address";
     text.classList.add("text_invalid");
   }
 });
 
 // Images API
-const image_primary_result = document.querySelector(".image_primary");
-const image_1_result = document.querySelector(".image_1");
-const image_2_result = document.querySelector(".image_2");
 const apiKey =
   "https://api.nasa.gov/planetary/apod?api_key=Dvrc5VermucA3JmvI8kcBfu2mQSDpDcngkEfMV4s&count=3";
 
 fetch(apiKey)
   .then((response) => response.json())
   .then((data) => {
-    image_primary_result.innerHTML = `<img src="${data?.[0]?.url}"/>`;
-    image_1_result.innerHTML = `<img src="${data?.[1]?.url}"/>`;
-    image_2_result.innerHTML = `<img src="${data?.[2]?.url}"/>`;
+    const images = document.querySelector(".images");
+    const smaller_images = document.querySelector(".smaller_images");
+    for (let i = 1; i < 3; i++) {
+      const img = document.createElement("img");
+      img.src = data?.[i]?.url;
+      img.classList.add("image");
+      smaller_images.append(img);
+    }
+    let img = document.createElement("img");
+    img.src = data?.[0]?.url;
+    img.classList.add("image_primary");
+    images.prepend(img);
   });
 
 // Mobile
